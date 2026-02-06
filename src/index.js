9333635c97fada86262d72a0c32ee54bd000d03f
ibanpare <13278805+ibanpare@users.noreply.github.com>
2026-02-06 23:10:38 +0100

    deleting files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-import { createTodo } from "./todoitem.js";
-
-const testTodo = createTodo("TestTitle", "Description", "12/12/2026", "High", "This is just a little test", ["firstCheck", "secondCheck", "thirdCheck"]);
-console.log(testTodo);
-
-for (const property in testTodo) {
-  console.log(`${property}: ${testTodo[property]}`);
-}
